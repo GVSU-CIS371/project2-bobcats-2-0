80b57320bdf80de571fa71f4dbfbf9df44a05c57
benSaun <saundebe@mail.gvsu.edu>
2024-10-13 18:22:28 -0400

    renderProdcts formatted better

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -16,45 +16,43 @@ function renderProducts(prods: Product[]): void {
         main.innerHTML = '';
         prods.forEach((prod)=>{
             //create new main for each item
-            const prodMain = document.createElement('main');
+            const prodMain = document.createElement('div');
             prodMain.classList.add('prod');
             //create element for name
-            const prodName = document.createElement('m1');
+            const prodName = document.createElement('div');
             prodName.textContent = prod.name;
-            //create element for description
-            const prodDescription = document.createElement('m2');
-            prodDescription.textContent = prod.description;
-            //create element for price
-            const prodPrice = document.createElement('m3');
-            prodPrice.textContent = `Price: $${prod.price.toFixed(2)}`;
-            prodPrice.classList.add('price');
-            //rating
-            const prodRating = document.createElement('m4');
-            prodRating.textContent = `Rating: ${prod.rating.toFixed(2)}`;
-            prodRating.classList.add('rating')
-            //stock
-            const prodStock = document.createElement('m5');
-            prodStock.textContent = `Stock: ${prod.stock.toFixed(2)}`;
-            prodStock.classList.add('stock')
-
             //image
             const prodImage = document.createElement('img');
             prodImage.src = prod.image;
             prodImage.alt = `${prod.name} image`;
             prodImage.width = 50;
             prodImage.height = 50;
-
+            //create element for description
+            const prodDescription = document.createElement('div');
+            prodDescription.textContent = prod.description;
+            //create element for price
+            const prodPrice = document.createElement('div');
+            prodPrice.textContent = `Price: $${prod.price.toFixed(2)}`;
+            prodPrice.classList.add('price');
+            //rating
+            const prodRating = document.createElement('span');
+            prodRating.textContent = `Rating: ${prod.rating.toFixed(2)}`;
+            prodRating.classList.add('rating')
+            //stock
+            const prodStock = document.createElement('div');
+            prodStock.textContent = `Stock: ${prod.stock.toFixed(2)}`;
+            prodStock.classList.add('stock')
             //category
-            const prodCategory = document.createElement('m6');
+            const prodCategory = document.createElement('div');
             prodCategory.textContent = prod.category;
 
             //append the elements to the item main
             prodMain.appendChild(prodName);
+            prodMain.appendChild(prodImage);
             prodMain.appendChild(prodDescription);
             prodMain.appendChild(prodPrice);
             prodMain.appendChild(prodRating);
             prodMain.appendChild(prodStock);
-            prodMain.appendChild(prodImage);
             prodMain.appendChild(prodCategory);
 
 
@@ -67,9 +65,35 @@ function renderProducts(prods: Product[]): void {
     
 
 function getByCategory(category: string): void {
-    // your code
-}
+    /*
+    const main = document.getElementById('main-container');
+    // throw error if main is empty
+    if(!main){
+        console.error('null');
+        return;
+    }
+    //clear the container
+    main.innerHTML = '';
+    //filter the products based on their category
+    const filtered = products.filter(prods => prods.category.toLowerCase() === category.toLowerCase());
+    //check if there were any products
+    if(filtered.length === 0){
+        const noProducts = document.createElement('a');
+        noProducts.textContent = `No items found`;
+        main.appendChild(noProducts);
+        return;
+    }   
+    //loop through filtered products
+    filtered.forEach((item) => {
+        const prodDiv = document.createElement('div');
+        prodDiv.classList.add('prod');
 
+        //name
+        const prodName = document.createElement('p')
+
+    })
+*/
+}
 function getByRating(minRating: number): void {
     // your code
 }
