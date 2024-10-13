f5c3b449f2cf3002d03cd47777901dd7c3a1b9a1
benSaun <saundebe@mail.gvsu.edu>
2024-10-13 19:11:04 -0400

    getByRating

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -133,7 +133,72 @@ function getByCategory(category: string): void {
 }
 
 function getByRating(minRating: number): void {
-    // your code
+   
+    const main = document.getElementById('main-container');
+    // throw error if main is empty
+    if(!main){
+        console.error('null');
+        return;
+    }
+    //clear the container
+    main.innerHTML = '';
+    //filter the products based on their rating
+    const filtered = products.filter(prods => prods.rating === minRating);
+    //check if there were any products
+    if(filtered.length === 0){
+        const noProducts = document.createElement('a');
+        noProducts.textContent = `No items found`;
+        main.appendChild(noProducts);
+        return;
+    }   
+    //loop through filtered products
+    filtered.forEach((prod) => {
+        const prodMain = document.createElement('main');
+        prodMain.classList.add('prod');
+
+        //name
+        const prodName = document.createElement('div');
+        prodName.textContent = prod.name;
+
+        //description
+        const prodDescription = document.createElement('div');
+        prodDescription.textContent = prod.description;
+
+        //image
+        const prodImage = document.createElement('img');
+        prodImage.src = prod.image;
+        prodImage.alt = `${prod.name} image`;
+        prodImage.width = 50;
+        prodImage.height = 50;
+
+        //price
+        const prodPrice = document.createElement('div');
+        prodPrice.textContent = `Price: $${prod.price.toFixed(2)}`;
+
+        //rating 
+        const prodRating = document.createElement('div');
+        prodPrice.textContent = `Rating: ${prod.rating.toFixed(2)}`;
+
+        //stock
+        const prodStock = document.createElement('div');
+        prodPrice.textContent = `Stock: $${prod.stock.toFixed(2)}`;
+
+        //category
+        const prodCategory =  document.createElement('div');
+        prodCategory.textContent = prod.category;
+
+        //append elements
+        prodMain.appendChild(prodName);
+        prodMain.appendChild(prodImage);
+        prodMain.appendChild(prodDescription);
+        prodMain.appendChild(prodPrice);
+        prodMain.appendChild(prodRating);
+        prodMain.appendChild(prodStock);
+        prodMain.appendChild(prodCategory);
+
+        //append main to container
+        main.appendChild(prodMain);
+    })
 }
 
 export { renderProducts, getByCategory, getByRating };
